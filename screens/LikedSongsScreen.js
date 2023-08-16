6f6d69f2a8723a9bf4d3995c0255258330c83687
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-08-16 13:37:37 +0300

    edited

--- screens/LikedSongsScreen.js
+++ screens/LikedSongsScreen.js
@@ -1,11 +1,189 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useState, useEffect, useContext, useRef } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { AntDesign } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Feather, FontAwesome } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import SongItem from "../components/SongItem";
+import { Player } from "../PlayerContext";
+import { BottomModal } from "react-native-modals";
+import { ModalContent } from "react-native-modals";
+import { Audio } from "expo-av";
+import { debounce } from "lodash";
 
 const LikedSongsScreen = () => {
+  const colors = [
+    "#27374D",
+    "#1D267D",
+    "#BE5A83",
+    "#212A3E",
+    "#917FB3",
+    "#37306B",
+    "#443C68",
+    "#5B8FB9",
+    "#144272",
+  ];
+  const navigation = useNavigation();
+  const [backgroundColor, setBackgroundColor] = useState("#0A2647");
+ // const { currentTrack, setCurrentTrack } = useContext(Player);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [searchedTracks, setSearchedTracks] = useState([]);
+  const [input, setInput] = useState("");
+  const [savedTracks, setSavedTracks] = useState([]);
+  const value = useRef(0);
+  const [currentSound, setCurrentSound] = useState(null);
+  const [progress, setProgress] = useState(null);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [totalDuration, setTotalDuration] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  async function getSavedTracks() {
+    const accessToken = await AsyncStorage.getItem("token");
+    const response = await fetch(
+      "https://api.spotify.com/v1/me/tracks?offset=0&limit=50",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+          limit: 50,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("failed to fetch the tracks");
+    }
+    const data = await response.json();
+    setSavedTracks(data.items);
+    console.log(savedTracks)
+    }
+    useEffect(() => {
+      getSavedTracks();
+    }, []);
+
+
   return (
-    <View>
-      <Text>LikedSongsScreen</Text>
-    </View>
+    <>
+       <LinearGradient colors={["#614385", "#516395"]} style={{ flex: 1 }}>
+       <ScrollView style={{ flex: 1, marginTop: 30 }}>
+            <Pressable
+              onPress={() => navigation.goBack()}
+              style={{ marginHorizontal: 10 }}>
+            <Ionicons name="arrow-back" size={24} color="white" />
+          </Pressable>
+          <Pressable
+            style={{
+              marginHorizontal: 10,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              marginTop: 9,
+            }}>
+               <Pressable
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                backgroundColor: "#42275a",
+                padding: 9,
+                flex: 1,
+                borderRadius: 3,
+                height: 38,
+              }}
+            >
+              <AntDesign name="search1" size={20} color="white" />
+              <TextInput
+                value={input}
+                onChangeText={(text) => handleInputChange(text)}
+                placeholder="Find in Liked songs"
+                placeholderTextColor={"white"}
+                style={{ fontWeight: "500",color:"white" }}
+              />
+            </Pressable>
+            <Pressable
+              style={{
+                marginHorizontal: 10,
+                backgroundColor: "#42275a",
+                padding: 10,
+                borderRadius: 3,
+                height: 38,
+              }}
+            >
+              <Text style={{ color: "white" }}>Sort</Text>
+            </Pressable>
+            </Pressable>
+            <View style={{ height: 30 }} />
+            <View style={{ marginHorizontal: 10 }}>
+              <Text style={{ fontSize: 18, fontWeight: "bold", color: "white" }}>
+                Liked Songs
+              </Text>
+              <Text style={{ color: "white", fontSize: 13, marginTop: 5 }}>
+                430 songs
+              </Text>
+            </View>
+
+            <Pressable
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginHorizontal: 10,
+              }}
+            >
+              <Pressable
+              style={{
+                width: 30,
+                height: 30,
+                borderRadius: 15,
+                backgroundColor: "#1DB954",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <AntDesign name="arrowdown" size={20} color="white" />
+            </Pressable>
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
+            >
+              <MaterialCommunityIcons
+                name="cross-bolnisi"
+                size={24}
+                color="#1DB954"
+              />
+              <Pressable
+                onPress={() =>{}}
+                style={{
+                  width: 60,
+                  height: 60,
+                  borderRadius: 30,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  backgroundColor: "#1DB954",
+                }}
+              >
+               <Entypo name="controller-play" size={24} color="white" />
+              </Pressable>
+            </View>
+            </Pressable>
+
+       </ScrollView>
+       </LinearGradient>
+    </>
   )
 }
 
